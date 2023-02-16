71007b057a41fd5207e72bf0a2c6950c6dca9b66
julysNICK <julystube@gmail.com>
2023-02-16 15:12:06 -0300

    add onboarding

--- lib/pages/onboarding/onboarding_pages.dart
+++ lib/pages/onboarding/onboarding_pages.dart
@@ -1,6 +1,8 @@
 import 'package:fintech_ui/theme.dart';
 import 'package:flutter/material.dart';
 
+import '../home/home_page.dart';
+
 class OnBoarding extends StatelessWidget {
   const OnBoarding({super.key});
 
@@ -9,14 +11,88 @@ class OnBoarding extends StatelessWidget {
     return Scaffold(
       backgroundColor: greenColor,
       body: SafeArea(
-          child: Column(
-        children: [
-          const SizedBox(
-            height: 80,
-          ),
-          Image.asset("assets/image_onboarding.png"),
-        ],
-      )),
+          bottom: false,
+          child: Stack(
+            children: [
+              Positioned(
+                bottom: -50,
+                left: -20,
+                child: Image.asset(
+                  "assets/image_hand.png",
+                  width: 350,
+                ),
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    Image.asset(
+                      "assets/image_onboarding.png",
+                      width: 335,
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      "Save your money",
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: medium,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "We help you to save your money",
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: light,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()));
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 48,
+                              vertical: 14,
+                            ),
+                            decoration: BoxDecoration(
+                              color: yellowColor,
+                              borderRadius: const BorderRadius.horizontal(
+                                left: Radius.circular(20),
+                              ),
+                            ),
+                            child: Text(
+                              "Next",
+                              style: blackTextStyle.copyWith(
+                                fontSize: 18,
+                                fontWeight: medium,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
